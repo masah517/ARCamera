b491c1c06a37c358f9bd767e35164e952463a19b
Masatoshi Takada <h5mtakada@gmail.com>
2021-12-25 00:30:49 +0900

    build scene

--- app/src/main/java/com/masa/arcamera/ui/MainFragment.kt
+++ app/src/main/java/com/masa/arcamera/ui/MainFragment.kt
@@ -3,18 +3,30 @@ package com.masa.arcamera.ui
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.SceneView
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.masa.arcamera.R
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    private lateinit var arFragment: ArFragment
+    private val arSceneView
+        get() = arFragment.arSceneView
+    private val arScene
+        get() = arSceneView.scene
 
-        }
-    }
+    private var model: Renderable? = null
+    private var modelView: ViewRenderable? = null
 
     companion object {
 
@@ -24,5 +36,39 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arFragment = (childFragmentManager.findFragmentById(R.id.arFragment) as ArFragment).apply {
+            setOnSessionConfigurationListener { session, config ->
+            }
+
+            setOnViewCreatedListener { arSceneView ->
+                arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL)
+            }
+
+            setOnTapArPlaneListener(::onPlaneTapped)
+        }
+    }
+
+    private fun onPlaneTapped(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
+        if (model == null ) {
+            Toast.makeText(context, "Loading AR Model...", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        arScene.addChild(AnchorNode(hitResult.createAnchor()).apply {
+                addChild(TransformableNode(arFragment.transformationSystem).apply {
+                    renderable = model
+                    renderableInstance.animate(true).start()
+                })
+            }
+        )
     }
 }
